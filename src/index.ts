89ca823bf58b7f9845e2c41c920f5d38424c1c80
hugostanley <stanleyhugo06@gmail.com>
2026-03-03 01:15:44 +0800

    chore: add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import { env } from "./config/env";
 import { chatRouter } from "./routers/chat";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/health_check", (req, res) => {
